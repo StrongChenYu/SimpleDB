ef7067bbc13598c547e265ca1f58c0582e53ee47
StrongChenYu <jarvis.chenyu@gmail.com>
2019-05-21 21:43:44 +0800

    stash test

---
+++ CS186-Proj2/src/main/java/simpledb/RecordId.java
@@ -0,0 +1,81 @@
+package simpledb;
+
+import java.io.Serializable;
+
+/**
+ * A RecordId is a reference to a specific tuple on a specific page of a
+ * specific table.
+ */
+public class RecordId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates a new RecordId referring to the specified PageId and tuple
+     * number.
+     * 
+     * @param pid
+     *            the pageid of the page on which the tuple resides
+     * @param tupleno
+     *            the tuple number within the page.
+     */
+    private PageId pid;
+    private int tupleno;
+
+    public RecordId(PageId pid, int tupleno) {
+        // some code goes here
+        this.pid = pid;
+        this.tupleno = tupleno;
+    }
+
+    /**
+     * @return the tuple number this RecordId references.
+     */
+    public int tupleno() {
+        // some code goes here
+        return tupleno;
+    }
+
+    /**
+     * @return the page id this RecordId references.
+     */
+    public PageId getPageId() {
+        // some code goes here
+        return pid;
+    }
+
+    /**
+     * Two RecordId objects are considered equal if they represent the same
+     * tuple.
+     * 
+     * @return True if this and o represent the same tuple
+     */
+    @Override
+    public boolean equals(Object o) {
+        // some code goes here
+        //throw new UnsupportedOperationException("implement this");
+
+        if(o instanceof RecordId){
+            RecordId tempRecordId = (RecordId)o;
+            if(tempRecordId.getPageId().equals(pid) && tempRecordId.tupleno() == tupleno) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * You should implement the hashCode() so that two equal RecordId instances
+     * (with respect to equals()) have the same hashCode().
+     * 
+     * @return An int that is the same for equal RecordId objects.
+     */
+    @Override
+    public int hashCode() {
+        // some code goes here
+        //throw new UnsupportedOperationException("implement this");
+        return pid.hashCode()+tupleno;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Simpledb 
+
+This is a implement of the database homework from berkeley.
+
+In my opinion, this project is friendly to beginners.
